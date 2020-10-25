f5f99958d593c064abc94db6c92cc0625add0924
SOVEREIGN <isimemeni@gmail.com>
2020-10-24 23:04:49 -0400

    added Player class method

--- war.py
+++ war.py
@@ -41,8 +41,37 @@ class Deck():
     def shuffle(self):
         random.shuffle(self.all_cards)
 
+    def deal_one(self):
+        return self.all_cards.pop()
+
 new_deck = Deck()
 new_deck.shuffle()
+mycard = new_deck.deal_one()
 
-for i in new_deck.all_cards:
-    print(i)
+class Player():
+    """
+    docstring
+    """
+    pass
+    def __init__(self, name):
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        return self.all_cards.pop(0)
+
+    def add_cards(self, new_cards):
+        if type(new_cards) == type([]):
+            # list of multiple Card objects
+            self.all_cards.extend(new_cards)
+        else:
+            # list of single card object
+            self.all_cards.append(new_cards)
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards'
+
+new_player = Player("Jose")
+new_player.add_cards(mycard)
+print(new_player)
+print(new_player.all_cards[0])
