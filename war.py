aa1940652d5cc3e5ca3b74c1ee0e51aed97c4582
SOVEREIGN <isimemeni@gmail.com>
2020-10-24 22:49:00 -0400

    added Deck class

--- war.py
+++ war.py
@@ -1,3 +1,4 @@
+import random
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
@@ -18,5 +19,30 @@ class Card():
         return self.rank + " of " + self.suit
 
 two_hearts = Card("Hearts", "Two")
+three_of_clubs = Card("Clubs","Three")
 
-print(values[two_hearts.rank])
+print(three_of_clubs.value)
+
+class Deck():
+    """
+    Deck Class
+    """
+    pass
+    def __init__(self):
+
+        self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                #Card Object
+                created_card = Card(suit, rank)
+                self.all_cards.append(created_card)
+
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+
+new_deck = Deck()
+new_deck.shuffle()
+
+for i in new_deck.all_cards:
+    print(i)
